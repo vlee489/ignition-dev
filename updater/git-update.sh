9c022512abb39fa1e5519abef8115c141822da1b
Vincent Lee <vlee1@live.co.uk>
2015-10-25 19:10:11 +0000

    Removes reinstall feature
    Removes reinstall feature due to it causing problems

--- updater/git-update.sh
+++ updater/git-update.sh
@@ -20,67 +20,10 @@ BaseRepositoryName="ignition-dev"
 #Where Ignition is stored and it's name
 Directory="/opt"
 IgnitionName="ignition"
+#Defines commands used
 1="nightly"
 
 if [ ! -z "$1" ]
-<<<<<<< HEAD
-  then
-      if [ ! -z "$2" ]
-        then
-            clear
-            echo "Reinstalling Ignition"
-            echo "Press (CTRL + C) to CANCEL"
-            echo "Starting in 3 seconds"
-            sleep 3
-            cd $Directory || exit
-            echo "Removing Ignition Install"
-            rm -rf ignition
-            clear
-            echo "Installing Ignition"
-            git clone -b nightly $GitRepositoryaddress/$BaseRepositoryName
-        else
-            clear
-            echo "Updating from Git Repository"
-            echo "Updating from Nightlies"
-            echo "Do NOT turn off"
-            echo "Updating in 3 seconds"
-            sleep 3
-            #If the directry it is trying to change
-            #to doesn't exsitst
-            #then it will exit the script.
-            cd $Directory/$IgnitionName || exit
-            #Change "nightly" if another branch is desiered
-            git pull nightly
-        fi
-  else
-    if [ ! -z "$2" ]
-      then
-          clear
-          echo "Reinstalling Ignition"
-          echo "Press (CTRL + C) to CANCEL"
-          echo "Starting in 3 seconds"
-          sleep 3
-          cd $Directory || exit
-          echo "Removing Ignition Install"
-          rm -rf ignition
-          clear
-          echo "Installing Ignition"
-          git clone -b stable $GitRepositoryaddress/$BaseRepositoryName
-      else
-          clear
-          echo "Updating from Git Repository"
-          echo "Updating from Nightlies"
-          echo "Do NOT turn off"
-          echo "Updating in 3 seconds"
-          sleep 3
-          #If the directry it is trying to change
-          #to doesn't exsitst
-          #then it will exit the script.
-          cd $Directory/$IgnitionName || exit
-          #Change "stable" if another branch is desiered
-          git pull stable
-      fi
-=======
 then
     clear
     echo "Updating from Git Repository"
@@ -107,5 +50,4 @@ else
    cd $Directory/$IgnitionName || exit
    #Change "stable" if another branch is desiered
    git pull stable
->>>>>>> parent of 0cc1c2c... [TASK](IC-21) Adds the ability to reinstall
 fi
