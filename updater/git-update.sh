b9b6932e4cda330acc6cd22e922c7d00b94df5ed
Vincent Lee <vlee1@live.co.uk>
2015-10-25 18:59:32 +0000

    Revert updates that adds reinstall

--- updater/git-update.sh
+++ updater/git-update.sh
@@ -12,6 +12,7 @@
 #that directory needs Root to access
 
 #Shortcuts
+#defines place where Ignition is stored.
 #------------------------
 #Git repos used to update
 GitRepositoryaddress="https://github.com/alexjstubbs"
@@ -19,11 +20,10 @@ BaseRepositoryName="ignition-dev"
 #Where Ignition is stored and it's name
 Directory="/opt"
 IgnitionName="ignition"
-#Defines commands used by the updater
 1="nightly"
-2="reinstall"
 
 if [ ! -z "$1" ]
+<<<<<<< HEAD
   then
       if [ ! -z "$2" ]
         then
@@ -80,4 +80,32 @@ if [ ! -z "$1" ]
           #Change "stable" if another branch is desiered
           git pull stable
       fi
+=======
+then
+    clear
+    echo "Updating from Git Repository"
+    echo "Updating from Nightlies"
+    echo "Do NOT turn off"
+    echo "Updating in 3 seconds"
+    sleep 3
+    #If the directry it is trying to change
+    #to doesn't exsitst
+    #then it will exit the script.
+    cd $Directory/$IgnitionName || exit
+    #Change "nightly" if another branch is desiered
+    git pull nightly
+else
+   clear
+   echo "Updading from Git Repository"
+    echo "Updating from Stable"
+   echo "Do NOT turn off"
+   echo "Updating in 3 seconds"
+   sleep 3
+   #If the directry it is trying to change
+   #to doesn't exsitst
+   #then it will exit the script.
+   cd $Directory/$IgnitionName || exit
+   #Change "stable" if another branch is desiered
+   git pull stable
+>>>>>>> parent of 0cc1c2c... [TASK](IC-21) Adds the ability to reinstall
 fi
